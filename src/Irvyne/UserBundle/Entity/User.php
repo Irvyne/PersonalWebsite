2ec2ac51ea629a3e1cb32530714b5377bc8108a8
Irvyne <thibaud.bardin@gmail.com>
2013-10-06 00:53:49 +0200

    Project Is Starting, Let's Go Baby ^^

---
+++ src/Irvyne/UserBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Irvyne\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // My Fucking Own Logic
+    }
+}
